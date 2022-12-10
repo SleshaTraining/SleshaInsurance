f4ab3e7aee72d2254da0f1378767fc0f041a54d6
viggy6 <vignesh6boppana@gmail.com>
2022-12-10 12:18:42 -0600

    added footer

--- ui/SleshaInsuranceUI/src/app/app.module.ts
+++ ui/SleshaInsuranceUI/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { WhyComponent } from './why/why.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    WhyComponent
+    WhyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
