f8d870700eda9c3c5f6da8b7d246cd29eab85f41
viggy6 <vignesh6boppana@gmail.com>
2022-12-12 12:44:09 -0600

    added unique constraint to email and phone number

--- services/userms/src/main/java/com/slesha/userms/entity/User.java
+++ services/userms/src/main/java/com/slesha/userms/entity/User.java
@@ -3,10 +3,12 @@ package com.slesha.userms.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +22,12 @@ public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer userId;
+    @Column(unique = true)
     private String emailId;
     private String userName;
     private String zip;
     private LocalDate dob;
+    @Column(unique = true)
     private String phone;
     private String password;
     
