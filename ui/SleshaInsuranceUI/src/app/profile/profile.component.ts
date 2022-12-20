9e7c1c70c458466634da6556418cfb56f97f2231
viggy6 <vignesh6boppana@gmail.com>
2022-12-20 13:46:48 -0600

    logout redirect implemented

--- ui/SleshaInsuranceUI/src/app/profile/profile.component.ts
+++ ui/SleshaInsuranceUI/src/app/profile/profile.component.ts
@@ -24,6 +24,7 @@ export class ProfileComponent implements OnInit {
           );
         },err=>{})
       }
+
     })
     
   }
--- ui/SleshaInsuranceUI/src/app/userhome/userhome.component.ts
+++ ui/SleshaInsuranceUI/src/app/userhome/userhome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from '../app.service';
 import { UserhomeService } from './userhome.service';
 
 @Component({
@@ -8,12 +10,17 @@ import { UserhomeService } from './userhome.service';
 })
 export class UserhomeComponent implements OnInit {
 
-  constructor(private service:UserhomeService) { }
+  constructor(private service:UserhomeService,private apService:AppService,private router:Router) { }
   plans:any=[];
   ngOnInit(): void {
     this.service.getPlans().subscribe(res=>{
       this.plans=res;
       this.plans.forEach((x:any)=>{x.img="../../assets/"+x.planId+'.jpg'});
+    })
+    this.apService.userLoggedIn.subscribe(res=>{
+      if(res==''){
+        this.router.navigateByUrl('/');
+      }
     })
   }
 
