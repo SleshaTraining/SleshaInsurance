5eb893baac78523673115329577fd7020325845b
viggy6 <vignesh6boppana@gmail.com>
2022-12-22 14:16:56 -0600

    completed kafka consumer and producer chain

--- services/processms/src/main/java/com/slesha/processms/ProcessmsApplication.java
+++ services/processms/src/main/java/com/slesha/processms/ProcessmsApplication.java
@@ -2,12 +2,17 @@ package com.slesha.processms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class ProcessmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessmsApplication.class, args);
+	}
+	@KafkaListener(topics = "enroll", groupId = "group-id")
+	public void listen(String message) {
+   		System.out.println("Received Messasge in group - group-id: " + message);
 	}
 
 }
