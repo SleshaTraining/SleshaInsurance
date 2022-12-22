cdc526b68131fdc15e003bf928557652a372321f
viggy6 <vignesh6boppana@gmail.com>
2022-12-22 13:19:43 -0600

    added processms

---
+++ services/processms/src/main/java/com/slesha/processms/ProcessmsApplication.java
@@ -0,0 +1,13 @@
+package com.slesha.processms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessmsApplication.class, args);
+	}
+
+}
