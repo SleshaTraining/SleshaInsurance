5d7b0fcf95b7af6604c3acb1833304b4b8778bc4
viggy6 <vignesh6boppana@gmail.com>
2022-12-21 20:28:17 -0600

    minor changes

--- services/planms/src/main/java/com/slesha/planms/repo/InsurancePlanRepo.java
+++ services/planms/src/main/java/com/slesha/planms/repo/InsurancePlanRepo.java
@@ -3,7 +3,9 @@ package com.slesha.planms.repo;
 import com.slesha.planms.entity.InsurancePlan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InsurancePlanRepo extends JpaRepository<InsurancePlan,Integer>{
     
 }
--- services/planms/src/main/java/com/slesha/planms/repo/UserPlanRepo.java
+++ services/planms/src/main/java/com/slesha/planms/repo/UserPlanRepo.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserPlanRepo extends JpaRepository<UserPlan,Integer>{
 
     @Query("select up from UserPlan up where up.user.emailId=?1")
