b3793bcd89572760e1436d9f47db43546b280480
viggy6 <vignesh6boppana@gmail.com>
2022-12-22 15:11:21 -0600

    login listener added

--- services/processms/src/main/java/com/slesha/processms/ProcessmsApplication.java
+++ services/processms/src/main/java/com/slesha/processms/ProcessmsApplication.java
@@ -11,7 +11,12 @@ public class ProcessmsApplication {
 		SpringApplication.run(ProcessmsApplication.class, args);
 	}
 	@KafkaListener(topics = "enroll", groupId = "group-id")
-	public void listen(String message) {
+	public void enrollListen(String message) {
+   		System.out.println("Received Messasge in group - group-id: " + message);
+	}
+
+	@KafkaListener(topics = "login", groupId = "group-id")
+	public void loginListen(String message) {
    		System.out.println("Received Messasge in group - group-id: " + message);
 	}
 
