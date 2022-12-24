4a086c9e1b1af1ccfb7293bd8ab09911d08fec30
viggy6 <vignesh6boppana@gmail.com>
2022-12-24 08:58:05 -0600

    complete integration with reverse proxy

--- ui/SleshaInsuranceUI/src/environments/environment.prod.ts
+++ ui/SleshaInsuranceUI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  userHost:'http://localhost:3333/userms',
-  planHost:'http://localhost:3333/planms'
+  userHost:'/userms',
+  planHost:'/planms'
 };
